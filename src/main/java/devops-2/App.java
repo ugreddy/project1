962cea6c24b962900df23ce6bbb871bc9cbc000a
govardana <govardana519@gmail.com>
2018-08-21 19:46:32 +0530

    project1

---
+++ src/main/java/devops-2/App.java
@@ -0,0 +1,13 @@
+package devops-2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
